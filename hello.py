a73769b0754374689d0948c408c3381e2e75a77d
Roderic <roderic@Roshiba.(none)>
2013-02-12 22:16:07 -0500

    Teamwork Task started. Tkinter will be used for now.

---
+++ hello.py
@@ -0,0 +1,23 @@
+"""
+Welcome Team
+
+A resource: http://effbot.org/tkinterbook/
+A better resource: http://pypi.python.org/pypi/PySide
+"""
+import Tkinter as tk
+
+app = tk.Tk()
+
+frame = tk.Frame(app)
+frame.pack()
+
+label = tk.Label(frame, text="""
+Add your name to this list.
+If you're feeling adventurous, add a widget.
+Or something else?
+---------------------------------------------------------------------------
+Harold
+""", borderwidth=5)
+label.pack(expand=1)
+
+tk.mainloop()
